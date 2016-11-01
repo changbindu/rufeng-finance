239a1159296e50dc2943ac2106d8a52afcfb7780
Du, Changbin <changbin.du@gmail.com>
2016-11-01 14:15:45 +0800

    use local tushare
    Signed-off-by: Du, Changbin <changbin.du@gmail.com>

--- tushare/rufeng-finance.py
+++ tushare/rufeng-finance.py
@@ -4,6 +4,7 @@ __author__ = 'Du, Changbin <changbin.du@gmail.com>'
 import sys
 if sys.version_info < (3, 4):
     raise RuntimeError('at least Python 3.4 is required to run')
+sys.path.append('tushare')
 
 import math
 import datetime
